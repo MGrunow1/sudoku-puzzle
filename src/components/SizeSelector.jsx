bb38cc97a27d35e9fa055e1df7f704ca96088743
MGrunow1 <100005756+MGrunow1@users.noreply.github.com>
2022-09-19 16:06:52 -0700

    fix(SizeSelector): Give correct styling

--- src/components/SizeSelector.jsx
+++ src/components/SizeSelector.jsx
@@ -1,39 +1,36 @@
 import { useContext } from "react";
 import { SudokuContext } from "../contexts/SudokuContext";
+import '../App.css'
 
 export default function SizeSelector() {
     const { resizeSudoku } = useContext(SudokuContext);
 
     return (
-        <>
-        <h2>Choose size of puzzle:</h2>
-        <button onClick={() => resizeSudoku({subrows: 2, subcols: 3})}>
-            6
-        </button>
-        <button onClick={() => resizeSudoku({subrows: 2, subcols: 4})}>
-            8
-        </button>
-        <button onClick={() => resizeSudoku({subrows: 3, subcols: 3})}>
-            9 x 9 (standard)
-        </button>
-        <button onClick={() => resizeSudoku({subrows: 4, subcols: 3})}>
-            12
-        </button>
-        <button onClick={() => resizeSudoku({subrows: 3, subcols: 5})}>
-            15
-        </button>
-        <button onClick={() => resizeSudoku({subrows: 4, subcols: 5})}>
-            16
-        </button>
-        <button onClick={() => resizeSudoku({subrows: 4, subcols: 5})}>
-            20
-        </button>
-        <button onClick={()=>resizeSudoku({subrows: 4, subcols: 6})}>
-            24
-        </button>
-        <button onClick={()=>resizeSudoku({subrows: 5, subcols: 5})}>
-            25
-        </button>
-        </>
+        <div style={{display: 'block'}}>
+            <h2>Choose size of puzzle:</h2>
+            <div className="ButtonContainer">
+                <button onClick={() => resizeSudoku({subrows: 2, subcols: 3})}>
+                    6 numbers
+                </button>
+                <button onClick={() => resizeSudoku({subrows: 2, subcols: 4})}>
+                    8 numbers
+                </button>
+                <button onClick={() => resizeSudoku({subrows: 3, subcols: 3})}>
+                    9 (standard)
+                </button>
+                <button onClick={() => resizeSudoku({subrows: 4, subcols: 3})}>
+                    12 numbers
+                </button>
+                <button onClick={() => resizeSudoku({subrows: 3, subcols: 5})}>
+                    15 numbers
+                </button>
+                <button onClick={() => resizeSudoku({subrows: 4, subcols: 5})}>
+                    16 numbers
+                </button>
+                <button onClick={() => resizeSudoku({subrows: 4, subcols: 5})}>
+                    20 numbers
+                </button>
+            </div>
+        </div>
     )
 }
