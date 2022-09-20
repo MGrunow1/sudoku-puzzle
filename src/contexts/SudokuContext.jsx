3999a994796b68b631ed42f7ded0b38024190f08
MGrunow1 <100005756+MGrunow1@users.noreply.github.com>
2022-09-20 09:29:39 -0700

    feat(SudokuContext): Swap rows to scramble

--- src/contexts/SudokuContext.jsx
+++ src/contexts/SudokuContext.jsx
@@ -33,10 +33,41 @@ const SudokuProvider = (props) => {
             // shift the values for the next column
             columnValues = columnValues.slice(subrows, maxNumber).concat(columnValues.slice(0,subrows));
         }
+        // swap random rows, to avoid the column pattern repeating
+        for(let bigRowNumber = 0; bigRowNumber < subcols; bigRowNumber++) {
+            if(subrows <= 2) {
+                // if there are just two rows, decide randomly whether to swap
+                if(Math.random() < .6) {
+                    const row1 = bigRowNumber * subrows;
+                    const row2 = 1 + (bigRowNumber * subrows);
+                    swapRows(row1, row2);
+                }
+            } else {
+                // always choose 2 rows to swap
+                const randomRow1 = Math.floor(Math.random() * subrows) + (bigRowNumber * subrows);
+                let randomRow2 = Math.floor(Math.random() * subrows) + (bigRowNumber * subrows);
+                // choose a different second row if they're the same
+                while(randomRow1 === randomRow2) {
+                    randomRow2 = Math.floor(Math.random() * subrows) + (bigRowNumber * subrows);
+                }
+                swapRows(randomRow1, randomRow2);
+            }
+            // helper function to swap rows in the same row of subgrids
+            function swapRows(row1, row2) {
+                const rowIndices1 = getRowIndices(row1, subcols, subrows);
+                const rowIndices2 = getRowIndices(row2, subcols, subrows);
+                for(let index = 0; index<maxNumber; index++) {
+                    // swap the values
+                    const temp = newPuzzle[rowIndices1[index]];
+                    newPuzzle[rowIndices1[index]] = newPuzzle[rowIndices2[index]];                        newPuzzle[rowIndices2[index]] = temp;
+                }
+            }
+        }
         setPuzzleSolution(newPuzzle);
         setPuzzleSize(size);
         setPuzzleCreated(true);
     }
+
     // get the list of index numbers for cells in a column
     const getColIndices = (colNum, subcols=puzzleSize.subcols, subrows=puzzleSize.subrows) => {
         const subGridSize = subcols * subrows;
@@ -47,6 +78,22 @@ const SudokuProvider = (props) => {
         for(let bigRowIndex = 0; bigRowIndex < subcols; bigRowIndex++) {
             for(let rowIndex = 0; rowIndex < subrows; rowIndex++) {
                 const index = colStart + (rowIndex * subcols) + (bigRowIndex * subGridSize * subrows);
+                indexArray.push(index);
+            }
+        }
+        return indexArray;
+    }
+    
+    // get the list of index numbers for cells in a row
+    const getRowIndices = (rowNum, subcols=puzzleSize.subcols, subrows=puzzleSize.subrows) => {
+        const subGridSize = subcols * subrows;
+        const bigRowNum = Math.floor(rowNum / subrows);
+        let rowStart = (rowNum % subrows) * subcols;
+        rowStart += bigRowNum * (subrows * subGridSize);
+        let indexArray = [];
+        for(let bigColIndex = 0; bigColIndex < subrows; bigColIndex++) {
+            for(let colIndex = 0; colIndex < subcols; colIndex++) {
+                const index = rowStart + colIndex + (bigColIndex * subGridSize);
                 indexArray.push(index);
             }
         }
