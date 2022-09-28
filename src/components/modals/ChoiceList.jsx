9705f33b921963b42ed1372ac59ac6b22561b78a
MGrunow1 <100005756+MGrunow1@users.noreply.github.com>
2022-09-28 14:59:18 -0700

    feat(ChoiceModal): Test choice

--- src/components/modals/ChoiceList.jsx
+++ src/components/modals/ChoiceList.jsx
@@ -2,22 +2,32 @@ import { useContext } from "react";
 import { SudokuContext } from "../../contexts/SudokuContext";
 import { CloseButton, NumberButton, NumberChooser } from "./ModalStyles"
 
-export default function ChoiceList() {
-    const { chosenCell, deselectCell, optionList } = useContext(SudokuContext);
+export default function ChoiceList({setWarning}) {
+    const { chosenCell, deselectCell, optionList, tryCellValue } = useContext(SudokuContext);
     
     function dontCloseModal(event) {
         event.stopPropagation();
+    }
+
+    function chooseOption(choice) {
+        const outcome = tryCellValue(choice);
+        if(outcome === 'safe') {
+            // successful, so close modal
+            deselectCell()
+        } else {
+            setWarning(outcome);
+        }
     }
     return (
         <NumberChooser
             css={chosenCell.horizontal}
             onClick={dontCloseModal}>
                 {optionList.map ((option, index) => (
-                    <NumberButton key={index}>
+                    <NumberButton key={index} onClick={() => chooseOption(option)}>
                         {option}
                     </NumberButton>
                 ))}
-                <NumberButton onClick={() => tryCellValue(null)}>
+                <NumberButton onClick={() => chooseOption(null)}>
                     empty
                 </NumberButton>
                 <CloseButton
--- src/components/modals/ChoiceModal.jsx
+++ src/components/modals/ChoiceModal.jsx
@@ -1,16 +1,21 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { SudokuContext } from "../../contexts/SudokuContext";
 import { ModalBackground } from "./ModalStyles"
 import ChoiceList from "./ChoiceList";
 
 export default function ChoiceModal() {
     const { chosenCell, deselectCell } = useContext(SudokuContext);
+    const [choiceWarning, setChoiceWarning] = useState('none');
     
     return (
         <ModalBackground
         top={chosenCell.moveTop}
         onClick={() => deselectCell()}>
-            <ChoiceList />
+            {choiceWarning === 'none' ? (
+                <ChoiceList setWarning={setChoiceWarning} />
+            ) : (
+                <></>
+            )}
         </ModalBackground>
     )
 }
