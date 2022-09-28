e021800009016eb9762d172cd4bf0fe46efd70a7
MGrunow1 <100005756+MGrunow1@users.noreply.github.com>
2022-09-28 11:53:04 -0700

    feat(ChoiceModal): Make modal closeable

--- src/components/modals/ChoiceModal.jsx
+++ src/components/modals/ChoiceModal.jsx
@@ -1,21 +1,32 @@
 import { useContext } from "react";
 import { SudokuContext } from "../../contexts/SudokuContext";
-import { ModalBackground, NumberButton, NumberChooser } from "./ModalStyles"
+import { CloseButton, ModalBackground, NumberButton, NumberChooser } from "./ModalStyles"
 
 export default function ChoiceModal() {
-    const { chosenCell, optionList } = useContext(SudokuContext);
+    const { chosenCell, deselectCell, optionList } = useContext(SudokuContext);
+    
+    function dontCloseModal(event) {
+        event.stopPropagation();
+    }
 
     return (
-        <ModalBackground top={chosenCell.moveTop}>
-            <NumberChooser css={chosenCell.horizontal}>
-        {optionList.map ((option, index) => (
-            <NumberButton key={index}>
-                {option}
-            </NumberButton>
-        ))}
-        <NumberButton>empty</NumberButton>
-        <NumberButton>cancel</NumberButton>
-    </NumberChooser>
+        <ModalBackground
+        top={chosenCell.moveTop}
+        onClick={() => deselectCell()}>
+            <NumberChooser css={chosenCell.horizontal}
+            onClick={dontCloseModal}
+            >
+                {optionList.map ((option, index) => (
+                    <NumberButton key={index}>
+                        {option}
+                </NumberButton>
+                ))}
+                <NumberButton>empty</NumberButton>
+                <CloseButton
+                onClick={() => deselectCell()}>
+                    cancel
+                </CloseButton>
+            </NumberChooser>
         </ModalBackground>
     )
 }
