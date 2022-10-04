8644ac14d3ad1fcff1c41acd8854b5cb0f1b98e0
MGrunow1 <100005756+MGrunow1@users.noreply.github.com>
2022-10-04 09:18:26 -0700

    fix(Grid): Label empty cells

--- src/components/grid/GridCell.jsx
+++ src/components/grid/GridCell.jsx
@@ -6,17 +6,18 @@ export default function GridCell({spaceNumber}) {
     const { cellType, userPuzzle, selectCell } = useContext(SudokuContext);
     const value = userPuzzle[spaceNumber];
     const type = cellType[spaceNumber];
+    const arialabel = value ? value.toString() : "blank"
 
     return (
         <>
             {type === 'clue' ? (
-            <CellButton type="button" disabled={true}>
-                {value}
-            </CellButton>
+                <CellButton type="button" disabled={true}>
+                    {value}
+                </CellButton>
             ) : (
-            <CellButton type="button" onClick={() => selectCell(spaceNumber)}>
-                {value}
-            </CellButton>
+                <CellButton type="button" aria-label={arialabel} onClick={() => selectCell(spaceNumber)}>
+                    {value}
+                </CellButton>
             )}
         </>
     )
