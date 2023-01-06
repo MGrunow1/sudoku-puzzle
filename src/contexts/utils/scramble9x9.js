0ee668e51da3e61b0c061c113154d47c155837fd
MGrunow1 <100005756+MGrunow1@users.noreply.github.com>
2023-01-05 16:33:07 -0800

    fix(scramble9x9): Start rows at correct location

--- src/contexts/utils/scramble9x9.js
+++ src/contexts/utils/scramble9x9.js
@@ -3,7 +3,7 @@ import { getRowIndices } from "./getIndices";
 const scramble9x9 = (puzzle) => {
     let newPuzzle = puzzle;
     const scramblePattern = chooseType();
-    const bigRowNumber = Math.floor(Math.random() * 3);
+    const bigRowNumber = Math.floor(Math.random() * 3) * 3;
     const row0 = getRowIndices(bigRowNumber, 3, 3);
     const row1 = getRowIndices(bigRowNumber + 1, 3, 3);
     const row2 = getRowIndices(bigRowNumber + 2, 3, 3);
